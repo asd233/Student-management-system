1b9c31f894be05753e21c06fab7fa9330261fd5e
asd233 <2053044516@qq.com>
2021-01-07 19:21:15 +0800

    添加了添加学生的功能

---
+++ add.php
@@ -0,0 +1,22 @@
+<?php
+//包含连接数据库的公共文件
+require_once("./conn.php");
+
+//判断表单是否合法提交(防止黑客攻击) 
+	//获取表单提交数据
+	$name	= $_POST['name'];
+	$sex	= $_POST['sex'];
+	$age 	= $_POST['age'];
+	$edu 	= $_POST['edu'];
+	$salary = $_POST['salary'];
+	$bonus 	= $_POST['bonus'];
+	$city 	= $_POST['city'];
+	//构建插入的SQL语句
+	$sql = "INSERT INTO student VALUES(null,'$name','$sex',$age,'$edu',$salary,$bonus,'$city')";
+	//判断SQL语句是否执行成功
+	if(mysqli_query($link,$sql))
+	{
+		echo "记录添加成功！";
+		die(); //中止程序向下运行
+	}
+?>
