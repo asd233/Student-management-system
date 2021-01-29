df441d0da5d62b7e4f34ccd5fc36ba3f58d8203f
asd233 <2053044516@qq.com>
2021-01-29 22:52:29 +0800

    修改数据库密码

--- conn.php
+++ conn.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";	//主机名  localhost:3306
 $db_port = "3306";		//端口号
 $db_user = "root";		//用户名
-$db_pass = "";		//密码
+$db_pass = "root";		//密码
 $db_name = "student";	//数据库名
 $charset = "utf8";		//字符集
 
